9a1e2a67688998cb69028e319d0214d20758dba0
njaimeh@wgu.edu <njaimeh@wgu.edu>
2024-02-19 04:40:37 -0800

    updates on Task 1

--- authenticate.js
+++ authenticate.js
@@ -40,3 +40,12 @@ exports.jwtPassport = passport.use(
 exports.verifyUser = passport.authenticate('jwt', { session: false });
 
 // TODO: Create the verifyAdmin middleware
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin) {
+        return next()
+    }
+    const err = new Error('You are not authorized to perform this operation!')
+    err.status = 403
+    res.setHeader('Content-Type', 'text/plain')
+    return next(err)
+}
